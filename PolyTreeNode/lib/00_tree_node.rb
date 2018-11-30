90d9453d6b2c33cb49bd25780459fc51521b1ffb
joedspin <jtdespinosa@gmail.com>
2018-11-30 16:38:20 -0500

    Create PolyTreeNode

--- PolyTreeNode/lib/00_tree_node.rb
+++ PolyTreeNode/lib/00_tree_node.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class PolyTreeNode
   attr_accessor :value, :children
   attr_reader :parent
@@ -10,24 +12,45 @@ class PolyTreeNode
 
   def parent=(node)
     if node.nil?
-        @parent = nil
+      @parent = nil
     else    
-        if self.parent
-            self.parent.children.reject! { |child| child == self }
-        end
-        @parent = node
-        node.children << self unless node.children.include?(self)
+      if self.parent
+        self.parent.children.reject! { |child| child == self }
+      end
+      @parent = node
+      node.children << self unless node.children.include?(self)
     end
   end
 
   def add_child(child)
     child.parent = self
-   end
+  end
 
-   def remove_child(child)
-     raise "not a valid child" if child.parent != self
-     child.parent = nil      
-   end
+  def remove_child(child)
+    raise "not a valid child" if child.parent != self
+    child.parent = nil      
+  end
+
+  def dfs(target)
+    return self if self.value == target
+    unless children.empty?
+      children.each do |node| 
+        result = node.dfs(target) 
+        return result if result != nil
+      end
+    end
+    nil
+  end
+
+  def bfs(target)
+    arr_q = []
+    arr_q << self
+    until arr_q.empty?
+      node = arr_q.shift
+      return node if node.value == target
+      arr_q.concat(node.children)
+    end
+  end
 
 end
 
